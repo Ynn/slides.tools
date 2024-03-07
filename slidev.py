19bf9348cf8d06f19e8c49a06513554a93516117
ynn <dev@4x.re>
2024-03-07 17:35:00 +0100

    wip

--- slidev.py
+++ slidev.py
@@ -1,4 +1,6 @@
 import docker
+from docker import DockerClient
+from docker.models.images import Image
 import logging
 import sys
 import os 
@@ -7,7 +9,9 @@ import time
 import subprocess
 import argparse
 
+VERSION = "1-0.48.0-beta.1"
 IMAGE_NAME = "nnynn/slidev"
+TAGGED_IMAGE_NAME = f"nnynn/slidev:{VERSION}"
 IMAGE_FILENAME = "slidev"
 
 def base_path():
@@ -28,32 +32,35 @@ def build_image(docker_context_path, tag):
     return image
 
 def build_and_store_image():
-    build_image(base_path(), "nnynn/slidev")
-    client = docker.from_env()
-    image = client.images.get("nnynn/slidev")
+    build_image(base_path(), TAGGED_IMAGE_NAME)
+    client:DockerClient = docker.from_env()
+    image:Image = client.images.get(TAGGED_IMAGE_NAME)
     image_xz_path = "slidev.tar.xz"
 
     print("Compressing the image...")
     # Utiliser lzma pour compresser directement le flux de données en .xz
-    with lzma.open(image_xz_path, 'wb', preset=lzma.PRESET_EXTREME) as lzma_file:
+    with lzma.open(image_xz_path, 'wb', preset=6) as lzma_file:
         for chunk in image.save(named=True):
             lzma_file.write(chunk)
 
-    print(f"L'image Docker a été compressée avec succès en {image_xz_path}")
+    print(f"Image has been successfully compressed {image_xz_path}")
 
+def load_image(client: DockerClient):
+    print(f"Loading container image {TAGGED_IMAGE_NAME}...")
+    try :
+        return client.images.get(TAGGED_IMAGE_NAME)
+    except docker.errors.ImageNotFound:
+        print("Image not found, loading it from file, will take at least 30s ...")
+        start = time.time()
+        with open(f"{base_path()}/{IMAGE_FILENAME}.tar.xz", "rb") as f:
+            images = client.images.load(f.read())    
 
-          
-def load_image(client):
-    start = time.time()
-    print("Loading container image...")
-    with open(f"{base_path()}/{IMAGE_FILENAME}.tar.xz", "rb") as f:
-        images = client.images.load(f.read())    
-    stop = time.time()
-    print("Image loaded in ", stop-start, " seconds")
-    
-    for image in images:
-        print(image.tags)
-        return image
+        stop = time.time()
+        print("Image loaded in ", stop-start, " seconds")
+        
+        for image in images:
+            print(image.tags)
+            return image
 
 def attach(container_id):
     command = ['docker', 'attach', container_id]
@@ -108,34 +115,53 @@ def get_dirname_filename(filename):
     print(f"Running on {filename} in {dirname}")
     return (dirname, filename)
 
-def slidev_run(filename) :
+def slidev_run(filename, args) :
     dirname,filename = get_dirname_filename(filename)
     command = ["npx","slidev", "slides/"+filename, "--remote"]
+    if args.tunnel :
+        command.append("--tunnel")
     slidev(dirname, command)
 
-def slidev_export(filename) :
+def slidev_export(filename, args) :
     dirname,filename = get_dirname_filename(filename)
     command = ["npx","slidev", "export", "slides/"+filename, "--format", "pdf", "--output", f"slides/{os.path.splitext(filename)[0]}"]
+    if args.with_clicks :
+        command.append("--with-clicks")
+    if args.with_toc :
+        command.append("--with-toc")
     slidev(dirname, command)
 
-def slidev_spa(filename) :
+def slidev_spa(filename, args) :
     dirname,filename = get_dirname_filename(filename)
     command = ["npx","slidev", "build", "slides/"+filename]
+    if args.with_clicks :
+        command.append("--with-clicks")
+    if args.with_toc :
+        command.append("--with-toc")
+    
     slidev(dirname, command)
 
 
 def main() :
     parser = argparse.ArgumentParser(description="Build and optionally run a Docker image")
+    parser.add_argument("-v", "--version", action="store_true", help="Show version of the script")
     parser.add_argument("-b", "--build", action="store_true", help="Build the Docker image")
     parser.add_argument("-r", "--run", help="File to compile")
     parser.add_argument("-e", "--export", help="Export the file to pdf")
-    parser.add_argument("-s", "--spa", help="Export the file to pdf")
-    parser.add_argument("--makebin", action="store_true", help="Make a binary of this docker script")
+    parser.add_argument("-c", "--with-clicks",action="store_true", help="Export pages for every clicks")
+    parser.add_argument("-t", "--tunnel",action="store_true", help="open a Cloudflare Quick Tunnel to make Slidev available on the internet")
+    parser.add_argument("-s", "--spa", help="Export the file to single web application")
+    parser.add_argument("--with-toc",action="store_true", help="Export pages with outline ")
+    parser.add_argument("--makebin", action="store_true", help="Make a binary of this docker script and image")
     
     args = parser.parse_args()
 
     docker_context_path = base_path()
     tag = IMAGE_NAME
+
+    if args.version:
+        print(f"script version :{VERSION.split('-')[0]} slidev version : {VERSION.split('-')[1]}")
+        sys.exit(0)
 
     if args.build:
         build_and_store_image()
@@ -156,13 +182,13 @@ def main() :
         sys.exit(0)
         
     if args.run :
-        slidev_run(args.run)
+        slidev_run(args.run, args)
         sys.exit(0)
     if args.export :
-        slidev_export(args.export)
+        slidev_export(args.export, args)
         sys.exit(0)
     if args.spa :
-        slidev_spa(args.spa)
+        slidev_spa(args.spa, args)
         sys.exit(0)
 if __name__ == "__main__":
     main()
